1583eb854f78908fb1624968c4dd64c03c393e18
LaurAlejandraC <lauralejandracg@gmail.com>
2016-11-27 01:25:16 -0500

    Get real Fourier Transform for the dataset

---
+++ audio_characteristics.py
@@ -0,0 +1,14 @@
+from scipy.fftpack import rfft
+
+
+def fourier_transform( audio_waves ):
+    words_fourier_transform = {}
+    for word in audio_waves:
+        words_fourier_transform[word] = []
+        for audio_wave in audio_waves[word]:
+            data = rfft(audio_wave)
+            data = abs(data)
+            words_fourier_transform[word].append(data)
+    return words_fourier_transform
+
+
