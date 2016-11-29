ce818006b692b941748cbbabf8a619ab999b687f
LaurAlejandraC <lauralejandracg@gmail.com>
2016-11-29 04:39:11 -0500

    Add test to classifier
    Avoids loading the classifier model

--- classifier.py
+++ classifier.py
@@ -1,7 +1,8 @@
 from pickle import load
 from sklearn.ensemble import RandomForestClassifier
-import cPickle as pickle
+import read_data, audio_characteristics
 import numpy as np
+import time
 
 MAX_LENGTH = 164052
 
@@ -36,4 +37,27 @@ prediction_model = RandomForestClassifier(n_estimators=10000, n_jobs=16, verbose
 prediction_model = prediction_model.fit(data, words)
 
 
-pickle.dump(prediction_model, open("prediction_model.p", "wb"))
+test_filenames = read_data.get_word_filenames('08')
+test_audio_waves = read_data.get_audio_waves(test_filenames)
+test_words_fourier_transform = audio_characteristics.fourier_transform(test_audio_waves)
+
+test_data, test_words = prepare_data(test_words_fourier_transform)
+print "Start prediction"
+start = time.time()
+test_data = test_data[:15]
+output = prediction_model.predict(test_data, n_jobs=16)
+end = time.time()
+print "End prediction"
+print end-start
+
+print output
+
+approved = 0.0
+for i in xrange(len(output)):
+    if test_words[i] == output[i]:
+        approved += 1.0
+
+print "Accuracy: " + str(approved)
+approved /= len(output)
+approved *= 100.0
+print "Accuracy percentage: " + str(approved)
