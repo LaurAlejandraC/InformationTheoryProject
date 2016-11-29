04405d9699cfb48335b4ab9c9d259ba914d5ee18
LaurAlejandraC <lauralejandracg@gmail.com>
2016-11-28 19:38:25 -0500

    Allow to load multiple groups of training data

--- read_data.py
+++ read_data.py
@@ -2,12 +2,12 @@ import os
 from scipy.io.wavfile import read, write
 
 
-def get_word_filenames():
+def get_word_filenames( groups ):
     filenames = os.listdir("Corpus")
     word_filenames = {}
     for current_file in filenames:
         word = current_file.split(".")
-        if word[1] == '01':
+        if word[1] in groups:
             if word[2] not in word_filenames:
                 word_filenames[word[2]] = [current_file]
             else:
@@ -23,8 +23,8 @@ def make_wav(data, outfile, samplerate):
     write(outfile, samplerate, data)
 
 
-def get_audio_waves():
-    word_filenames = get_word_filenames()
+def get_audio_waves( word_filenames ):
+
     audio_waves = {}
     for word in word_filenames:
         audio_waves[word] = []
--- save_data.py
+++ save_data.py
@@ -1,7 +1,10 @@
 import audio_characteristics, read_data
 import cPickle as pickle
 
-audio_waves = read_data.get_audio_waves()
+word_filenames = read_data.get_word_filenames(['01'])
+pickle.dump(word_filenames, open("word_filenames.p", "wb"))
+
+audio_waves = read_data.get_audio_waves( word_filenames )
 pickle.dump(audio_waves, open("audio_waves.p", "wb"))
 
 words_fourier_transform = audio_characteristics.fourier_transform(audio_waves)
