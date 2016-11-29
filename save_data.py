bcd73990b23c57777b0122c7f378965a7bcc3bb6
LaurAlejandraC <lauralejandracg@gmail.com>
2016-11-29 14:12:13 -0500

    fix save data

--- save_data.py
+++ save_data.py
@@ -1,12 +1,12 @@
 import audio_characteristics, read_data
 import cPickle as pickle
 
-word_filenames = read_data.get_word_filenames(['01','02','03','04'])
-pickle.dump(word_filenames, open("word_filenames_test.p", "wb"))
+word_filenames = read_data.get_word_filenames(['01'])
+pickle.dump(word_filenames, open("word_filenames.p", "wb"))
 
 audio_waves = read_data.get_audio_waves( word_filenames )
-pickle.dump(audio_waves, open("audio_waves_test.p", "wb"))
+pickle.dump(audio_waves, open("audio_waves.p", "wb"))
 
 words_fourier_transform = audio_characteristics.fourier_transform(audio_waves)
-pickle.dump(words_fourier_transform, open("words_fourier_transform_test.p", "wb"))
+pickle.dump(words_fourier_transform, open("words_fourier_transform.p", "wb"))
 
